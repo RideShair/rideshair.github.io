a82354987527c6db7ac884c916fabf6b6f8ecd27
RideShair <65855628+RideShair@users.noreply.github.com>
2020-05-28 12:29:21 +0100

    RideShair Corporate Site

---
+++ js/slider.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    var item = '#slider-1 .carousel-item';
+    var item_inner = "#slider-1 .carousel-inner";
+    classes = GetUnique(item);
+    setcss(classes, item, item_inner);
+
+
+    var item_1 = '#slider-2 .carousel-item';
+    var item_inner_1 = "#slider-2 .carousel-inner";
+    classes = GetUnique(item_1);
+    setcss(classes, item_1, item_inner_1);
+});
